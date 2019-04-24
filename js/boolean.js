9c6680ea8a1e3dd9784ca07e4b0e3b7ea88df213
Andy Callahan <amandafillpot@icloud.com>
2019-04-23 20:08:00 -0700

    added boolean explanation

---
+++ js/boolean.js
@@ -0,0 +1,12 @@
+// Boolean data type
+var a = true; // 1
+var b = false; // 0
+
+console.log(12 == '12'); // true
+console.log(24 === '24'); // false
+
+console.log(isNaN(2000)); // false
+console.log(isNaN('2000')); // false
+console.log(isNaN('abc')); // true
+console.log(isNaN(true)); // false
+console.log(isNaN('04/23/2019')); // true
