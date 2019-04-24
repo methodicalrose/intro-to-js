f2345fc8ba2017fff94e7b81663e829e7e617c8c
Andy Callahan <amandafillpot@icloud.com>
2019-04-23 19:04:12 -0700

    added number and string js files

---
+++ js/number.js
@@ -0,0 +1,36 @@
+// Number data type
+var a = 10;
+console.log(a);
+var b = 100;
+console.log(b);
+var a = 20;
+console.log(a);
+var c = 0x1E; // hex value of 30
+console.log(c);
+var d = 0.5;
+console.log(d);
+
+// Addition
+var sum = a + b;
+console.log(sum);
+
+// Subtraction
+var diff = d - c;
+console.log(diff);
+
+// Multiplication
+var mult = a * d;
+console.log(mult);
+
+// Division
+var div = b / a;
+console.log(div);
+
+// Modulus (Remainder)
+var mod = b % a;
+console.log(mod);
+var mod = b % c;
+console.log(mod);
+
+var e = '5'; // not a number; is a string
+console.log(typeof e); // returns the type of variable e
