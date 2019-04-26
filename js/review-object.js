324d041847c9bd6d1699543f5705549216a1d1b6
Andy Callahan <amandafillpot@icloud.com>
2019-04-25 19:49:04 -0700

    reviewing objects

---
+++ js/review-object.js
@@ -0,0 +1,10 @@
+var fullName = {
+    firstName: 'Andy',
+    lastName: 'Callahan'
+};
+console.log(fullName);
+console.log(fullName.firstName);
+console.log(fullName.lastName);
+
+console.log(fullname.first + ' ' + fullname.last);
+console.log(fullName.first.concat(' ', fullName.last));
